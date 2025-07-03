0c3ac86509f2baf0b6748fb192f21d6e023634f7
Pro Counsellor <counsellorpro@gmail.com>
2025-07-04 02:40:44 +0530

    ignored courseandState from jwt

--- ProCounsellor/src/main/java/com/catalyst/ProCounsellor/config/JwtAuthenticationFilter.java
+++ ProCounsellor/src/main/java/com/catalyst/ProCounsellor/config/JwtAuthenticationFilter.java
@@ -46,6 +46,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
         String path = request.getRequestURI();
-        return path.startsWith("/api/auth");
+        return path.startsWith("/api/auth") || path.startsWith("/api/courseAndState");
     }
 }
